fb019b56ace0fcb5256cf707d0c65c4f0cd7071b
Mateus <Soares>
2025-01-06 23:07:36 -0300

    Adicionando botão de envio de mensagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Message from './components/Message/Message'
 import './css/tailwind.css'
 
 function App() {
 
+  const [mensagens, setMensagens] = useState([]);
+  const [mensagem, setMensagem] = useState("");
+
+  function novaMensagem(e: event)
+  {
+    e.preventDefault();
+    console.log(mensagem);
+    const user = "Mateus";
+    const data = new Date(Date.now()).toLocaleString('pt-BR');
+    setMensagens(mensagens => [...mensagens, {user, data, mensagem}]);
+    
+  }
+
+  const handleMessage = (event) => {
+    setMensagem(event.target.value);
+  };
+  
   return (
     <>
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 h-96 overflow-x-auto">
-          
-          <Message name={"Mateus Soares"} time={"15:10"} message={"Testando 123"}/>
+        {mensagens.map(mensagem => (<Message name={mensagem.user} time={mensagem.data} message={mensagem.mensagem}/> ))}
+          {/* <Message name={"Mateus Soares"} time={"15:10"} message={"Testando 123"}/>
           <Message name={"Kinzé"} time={"15:10"} message={"Testando 123"}/>
           <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/>
           <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/>
@@ -17,14 +35,14 @@ function App() {
           <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/>
           <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/>
           <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/>
-          <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/>
+          <Message name={"Tonho"} time={"15:10"} message={"Testando 123"}/> */}
         
         </div>
         <form>   
             <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
-                <input type="search" id="search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type a message" required />
-                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+                <input type="text" id="search" value={mensagem} onChange={handleMessage} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type a message" required />
+                <button type="submit" onClick={novaMensagem} className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
             </div>
         </form>    
     </>
